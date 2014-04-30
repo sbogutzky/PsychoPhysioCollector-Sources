13cc4904dba4a4dc37c315cff3f734a413c1b138
Simon Bogutzky <simon.bogutzky@t-online.de>
2014-04-30 11:09:02 +0200

    Man kann eine Tabellenzeile hinzufügen

--- app/src/main/java/de/bogutzky/datacollector/app/MainActivity.java
+++ app/src/main/java/de/bogutzky/datacollector/app/MainActivity.java
@@ -5,21 +5,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.SimpleCursorAdapter;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-
 
 public class MainActivity extends ListActivity {
 
     // This is the Adapter being used to display the list's data
     ArrayAdapter adapter;
     ArrayList<String> sensors;
+    //MultiShimmerTemplateService multiShimmerTemplateService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Intent intent = new Intent(this, MultiShimmerTemplateService.class);
+        //startService(intent);
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getSensors());
         setListAdapter(adapter);
@@ -40,6 +40,12 @@ public class MainActivity extends ListActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_add_sensor) {
+            getSensors().add("Sensor " + getSensors().size());
+            adapter.notifyDataSetChanged();
+
+            //mService.mShimmerConfigurationList.add(new ShimmerConfiguration("Device", "", mService.mShimmerConfigurationList.size(), Shimmer.SENSOR_ACCEL, 51.2, -1, -1, -1,-1,-1,-1,-1,-1,0,-1));
+            //updateShimmerConfigurationList(mService.mShimmerConfigurationList);
+
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -48,7 +54,6 @@ public class MainActivity extends ListActivity {
     public ArrayList<String> getSensors() {
         if (sensors == null) {
             sensors = new ArrayList<String>();
-            sensors.add(getString(R.string.no_sensors));
         }
         return sensors;
     }
