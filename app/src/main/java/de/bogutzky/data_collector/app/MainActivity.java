888f09e51858ca626f66000d230c80643c4f1fbf
Simon Bogutzky <simon.bogutzky@t-online.de>
2014-05-06 15:14:48 +0200

    Connect to Shimmer implementiert

--- app/src/main/java/de/bogutzky/data_collector/app/MainActivity.java
+++ app/src/main/java/de/bogutzky/data_collector/app/MainActivity.java
@@ -4,24 +4,96 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.shimmerresearch.android.Shimmer;
+import com.shimmerresearch.driver.ObjectCluster;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends ListActivity {
 
     private static final String TAG = "MainActivity";
+    public static final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+
+            switch (msg.what) {
+                case Shimmer.MESSAGE_READ:
+                    if ((msg.obj instanceof ObjectCluster)) {
+                        ObjectCluster objectCluster = (ObjectCluster) msg.obj;
+                        Log.d(TAG, "Data from: " + objectCluster.mBluetoothAddress);
+                    }
+                    break;
+                case Shimmer.MESSAGE_TOAST:
+                    Log.d(TAG, msg.getData().getString(Shimmer.TOAST));
+                    break;
+                case Shimmer.MESSAGE_STATE_CHANGE:
+                    String bluetoothAddress = "None";
+                    if ((msg.obj instanceof ObjectCluster)) {
+                        ObjectCluster objectCluster = (ObjectCluster) msg.obj;
+                        bluetoothAddress = objectCluster.mBluetoothAddress;
+                    }
+                    switch (msg.arg1) {
+                        case Shimmer.STATE_CONNECTED:
+                            Log.d(TAG, "Connected: " + bluetoothAddress);
+                            break;
+                        case Shimmer.STATE_CONNECTING:
+                            Log.d(TAG, "Connecting: " + bluetoothAddress);
+                            break;
+                        case Shimmer.STATE_NONE:
+                            Log.d(TAG, "None State: " + bluetoothAddress);
+//                            mMultiShimmer.remove(((ObjectCluster) msg.obj).mBluetoothAddress);
+//                            if (mBluetoothAddresstoConnect.size() != 0) {
+//                                mBluetoothAddresstoConnect.remove(0);
+//                                mDeviceNametoConnect.remove(0);
+//                                if (mBluetoothAddresstoConnect.size() != 0) {
+//                                    MultiShimmerTemplateService service = mService.get();
+//                                    service.connectShimmer(mBluetoothAddresstoConnect.get(0), mDeviceNametoConnect.get(0));
+//                                }
+//                            }
+                            break;
+                        case Shimmer.MSG_STATE_FULLY_INITIALIZED:
+                            Log.d(TAG, "Fully initialized: " + bluetoothAddress);
+                            getConnectedShimmers().put(bluetoothAddress, getShimmers().get(bluetoothAddress));
+                            break;
+                    }
+                    break;
+                case Shimmer.MESSAGE_PACKET_LOSS_DETECTED:
+                    Log.d(TAG, "Packet loss detected");
+                    break;
+            }
+        }
+    };
     private static final int MSG_BLUETOOTH_ADDRESS = 1;
+    private static HashMap<String, Object> shimmers;
+    private static HashMap<String, Object> connectedShimmers;
+    private ArrayAdapter adapter;
+    private ArrayList<String> bluetoothAddresses;
+    private boolean noShimmerIsStreaming = true;
 
-    ArrayAdapter adapter;
-    ArrayList<String> bluetoothAddresses;
-    boolean noDeviceIsStreaming = true;
+    public static HashMap<String, Object> getShimmers() {
+        if (shimmers == null) {
+            shimmers = new HashMap<String, Object>(4);
+        }
+        return shimmers;
+    }
 
+    public static HashMap<String, Object> getConnectedShimmers() {
+        if (connectedShimmers == null) {
+            connectedShimmers = new HashMap<String, Object>(4);
+        }
+        return connectedShimmers;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +101,14 @@ public class MainActivity extends ListActivity {
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getBluetoothAddresses());
         setListAdapter(adapter);
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, "Connect to: " + getBluetoothAddresses().get(i));
+                connectShimmer(getBluetoothAddresses().get(i), "test");
+            }
+        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,7 +143,7 @@ public class MainActivity extends ListActivity {
                     Log.d(TAG, "Bluetooth Address: " + bluetoothAddress);
 
                     // Check if the bluetooth address has been previously selected
-                    boolean isNewAddress = !bluetoothAddresses.contains(bluetoothAddress);
+                    boolean isNewAddress = !getBluetoothAddresses().contains(bluetoothAddress);
 
                     if (isNewAddress) {
                         addBluetoothAddress(bluetoothAddress);
@@ -85,16 +163,30 @@ public class MainActivity extends ListActivity {
     }
 
     public void addBluetoothAddress(String bluetoothAddress) {
-        bluetoothAddresses.add(bluetoothAddress);
+        getBluetoothAddresses().add(bluetoothAddress);
         adapter.notifyDataSetChanged();
     }
 
     public void findBluetoothAddress() {
-        if (noDeviceIsStreaming) {
+        if (noShimmerIsStreaming) {
             Intent intent = new Intent(this, DeviceListActivity.class);
             startActivityForResult(intent, MSG_BLUETOOTH_ADDRESS);
         } else {
             Toast.makeText(this, getString(R.string.ensure_no_device_is_streaming), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void connectShimmer(String bluetoothAddress, String deviceName) {
+        Shimmer shimmer = new Shimmer(this, handler, deviceName, false);
+        if (!getShimmers().containsKey(bluetoothAddress)) {
+            getShimmers().put(bluetoothAddress, shimmer);
+        } else {
+            Log.d(TAG, "Already added");
+        }
+        if (!getConnectedShimmers().containsKey(bluetoothAddress)) {
+            shimmer.connect(bluetoothAddress, "default");
+        } else {
+            Log.d(TAG, "Already connected");
         }
     }
 }
