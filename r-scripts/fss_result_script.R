e1de96bd6938510f61e5ae98c36fb6157208ecc4
Simon Bogutzky <simon.bogutzky@t-online.de>
2014-06-10 13:13:11 +0200

    Überflüssige Skripte gelöscht

--- r-scripts/fss_result_script.R
+++
@@ -1,9 +0,0 @@
-setwd("~/Entwicklung/projects/bogutzky/repositories/data-collector-android/r-scripts")
-source("/Users/simonbogutzky/Entwicklung/projects/bogutzky/repositories/flow-gait-analysis/r-code/scripts/final/functions/fss-functions.R")
-
-directory.name <- "2014-05-19_17-12-43"
-
-scale <- read.csv(paste("../data/", directory.name, "/scale.csv", sep =""))
-results <- CalculateFlowShortScaleFactors(cbind(scale[3:18], scale$System.Timestamp.01, scale$System.Timestamp.02))
-summary(results)
-
