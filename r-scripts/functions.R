4375904dbc55235a9ec92b6f839029167a963a56
Simon Bogutzky <simon.bogutzky@t-online.de>
2014-06-13 13:35:42 +0200

    Eigene HRV Funktionen hinzugefügt

--- r-scripts/functions.R
+++ r-scripts/functions.R
@@ -131,6 +131,150 @@ CalculateFlowShortScaleFactors <- function(fss.items) {
   return(fss.factors)
 }
 
+CalculateMeanRR <- function(rr.intervals, round.digits = 4) { 
+  return(round(mean(rr.intervals) * 1000, round.digits))
+}
+
+CalculateSDNN <- function(rr.intervals, round.digits = 4) {
+  return(round(sd(rr.intervals) * 1000, round.digits))
+}
+
+CalculateRMSSD <- function(rr.intervals, round.digits = 4) {
+  rr.differences <- abs(diff(rr.intervals))
+  return(round(sqrt(sum(rr.differences^2) / length(rr.differences)) * 1000, round.digits))
+}
+
+CalculateHRVFrequencyDomainParameters <- function(rr.times, rr.intervals, band.vlf, band.lf, band.hf, interpolation.rate, window.width, window.overlap) {
+  
+  # Calculation of the fourier transformation required equidistant values of RR intervals
+  resampled.data          <- Interpolate(rr.times, rr.intervals, interpolation.rate, "linear")
+  resampled.rr.intervals  <- resampled.data$y
+  print(length(resampled.rr.intervals))
+  
+  # Periodogram
+  periodogram <- ComputeWelchPeriodogram(resampled.rr.intervals, interpolation.rate, window.width, window.overlap)
+
+  return(periodogram)
+  #   
+#   # Find the indexes corresponding to the VLF, LF, and HF bands
+#   indexes.vlf <- (periodogram$Freq >= band.vlf[1]) & (periodogram$Freq <= band.vlf[2])
+#   indexes.lf  <- (periodogram$Freq >= band.lf[1]) & (periodogram$Freq <= band.lf[2])
+#   indexes.hf  <- (periodogram$Freq >= band.hf[1]) & (periodogram$Freq <= band.hf[2])
+#   
+#   # Find peaks
+#   # VLF peak
+#   freq.temp <- periodogram$Freq[indexes.vlf]
+#   spec.temp <- periodogram$Spec[indexes.vlf]
+#   peaks     <- FindPeaks(spec.temp)
+#   if (nrow(peaks) != 0) {
+#     peak.max.index  <- which(peaks$Maxima == max(peaks$Maxima))        
+#     peak.vlf        <- freq.temp[peaks$Index[peak.max.index]]
+#   } else {
+#     peak.max.index  <- which(spec.temp == max(spec.temp))        
+#     peak.vlf        <- freq.temp[peak.max.index]
+#   }
+#   
+#   if(length(peak.vlf) == 0) peak.vlf = 0
+#   
+#   # LF Peak
+#   freq.temp <- periodogram$Freq[indexes.lf]
+#   spec.temp <- periodogram$Spec[indexes.lf]
+#   peaks     <- FindPeaks(spec.temp)
+#   if (nrow(peaks) != 0) {
+#     peak.max.index  <- which(peaks$Maxima == max(peaks$Maxima))        
+#     peak.lf         <- freq.temp[peaks$Index[peak.max.index]]
+#   } else {
+#     peak.max.index  <- which(spec.temp == max(spec.temp))        
+#     peak.lf         <- freq.temp[peak.max.index]
+#   }
+#   
+#   # HF Peak
+#   freq.temp <- periodogram$Freq[indexes.hf]
+#   spec.temp <- periodogram$Spec[indexes.hf]
+#   peaks     <- FindPeaks(spec.temp)
+#   if (nrow(peaks) != 0) {
+#     peak.max.index  <- which(peaks$Maxima == max(peaks$Maxima))        
+#     peak.hf         <- freq.temp[peaks$Index[peak.max.index]]
+#   } else {
+#     peak.max.index  <- which(spec.temp == max(spec.temp))        
+#     peak.hf         <- freq.temp[peak.max.index]
+#   }
+#   
+#   # Calculate raw areas (power under curve), within the freq bands (ms^2)
+#   require(pracma)
+#  
+#   area.vlf    <- trapz(periodogram$Freq[indexes.vlf], periodogram$Spec[indexes.vlf])
+#   area.lf     <- trapz(periodogram$Freq[indexes.lf], periodogram$Spec[indexes.lf])
+#   area.hf     <- trapz(periodogram$Freq[indexes.hf], periodogram$Spec[indexes.hf])
+#   area.total  <- area.vlf + area.lf + area.hf
+#   
+#   # Calculate areas relative to the total area (%)
+#   p.vlf <- (area.vlf / area.total) * 100
+#   p.lf  <- (area.lf / area.total)  * 100
+#   p.hf  <- (area.hf / area.total) * 100
+#   
+#   # Calculate normalized areas (relative to HF+LF, n.u.)
+#   n.lf <- area.lf / (area.lf + area.hf) * 100
+#   n.hf <- area.hf / (area.lf + area.hf) * 100
+#   
+#   # Calculate LF/HF ratio
+#   lfhf <- area.lf / area.hf
+#   
+#   # Create output structure
+#   result.area.vlf   <- round(area.vlf * 10^6 * 100) / 100
+#   result.area.lf    <- round(area.lf * 10^6 * 100) / 100
+#   result.area.hf    <- round(area.hf * 10^6 * 100) / 100
+#   result.area.total <- round(area.total * 10^6 * 100) / 100
+#   
+#   result.p.vfl      <- round(p.vlf * 100) / 100
+#   result.p.lf       <- round(p.lf * 100) / 100
+#   result.p.hf       <- round(p.hf * 100) / 100
+#   result.n.lf       <- round(n.lf * 100) / 100
+#   result.n.hf       <- round(n.hf * 100) / 100
+#   result.lfhf       <- round(lfhf * 100) / 100
+#   result.peak.vlf   <- round(peak.vlf * 100) / 100
+#   result.peak.lf    <- round(peak.lf * 100) / 100
+#   result.peak.hf    <- round(peak.hf * 100) / 100
+#   
+#   # Create result data frame
+#   result.hrv.frequency.domain.parameters <- data.frame(AVLF = result.area.vlf, ALF = result.area.lf, AHF = result.area.hf, ATotal = result.area.total, PVLF = result.p.vfl, PLF = result.p.lf, PHF = result.p.hf, NLF = result.n.lf, NHF = result.n.hf, LFHF = result.lfhf, PeakVLF = result.peak.vlf, PeakLF = result.peak.lf, PeakHF = result.peak.hf)
+#   return(result.hrv.frequency.domain.parameters)
+}
+
+Interpolate <- function(x, y, interpolation.rate, method = c("linear", "nearest", "pchip", "cubic", "spline")) {
+  
+  # Caluculate xi
+  xi <- seq(x[1], x[length(x)], by = 1/interpolation.rate)
+  
+  # Interpolate
+  require(signal)
+  yi <- interp1(x, y, xi, method = method)
+  return(data.frame(x = xi, y = yi))
+}
+
+ComputeWelchPeriodogram <- function(x, interpolation.rate, window.width, window.overlap) {
+  
+  # Calculate samples in DFT
+  nfft <- window.width * interpolation.rate
+  print(nfft)
+    
+  # Calculte overlap
+  noverlap <- nfft * window.overlap
+  print(noverlap)
+  
+  # Truncate length (like Matlab)
+  trunc.x.length <- length(x) %% noverlap
+  print(trunc.x.length)
+  x <- x[1:(length(x) - trunc.x.length)]
+  
+  # Compute periodogram using the Welch (1967) method
+  require(oce)
+  print(summary(x))
+  w <- pwelch(x, noverlap = noverlap, nfft = nfft, fs = interpolation.rate, plot = T, debug = T)
+  #w <- pwelch(x, 7, .5, fs = interpolation.rate, plot = T, debug = T)
+  return(periodogram <- data.frame(Freq = w$freq, Spec = w$spec))
+}
+
 PlotRelation <- function(x, y, ..., summary = F) {
   
   if(length(x) > 1) {
