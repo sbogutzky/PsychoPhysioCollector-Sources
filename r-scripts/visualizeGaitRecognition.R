3501a792fc9a4cd5ee9d4f40d76ccc42aa9b088c
Philipp Marsch <pmarsch@stud.hs-bremen.de>
2014-09-10 23:39:23 -0400

    fixed double plot issue

--- r-scripts/visualizeGaitRecognition.R
+++ r-scripts/visualizeGaitRecognition.R
@@ -6,16 +6,11 @@
 #
 
 ###### Set Directory and Filenames
-setwd("~/Desktop/gait test 06.09.2014")
+setwd("~/Desktop/gait test 10.09.2014")
 data.name <- "motion-data"
 event.name <- "motion-event-data"
 name <- "philipp"
 require("stringr")
-
-# dates <- c("2014-09-06-t14-18-32", 
-#            "2014-09-06-t14-19-13", 
-#            "2014-09-06-t14-21-03",
-#            "2014-09-06-t14-30-49")
 
 ####### TODO read dates out of filenames
 # Problem: Not date can differ in one minute due to saving duration
@@ -26,25 +21,25 @@ dates <- dates[!duplicated(dates)]
 ###### Print Graphs
 for (date in dates) {
   
-  file.name <- paste(date, "-", data.name, "-", name, ".csv", sep="")
-  if (file.exists(file.name)) {
-    leg.data <- read.csv(file.name)  
+  file.name1 <- paste(date, "-", data.name, "-", name, ".csv", sep="")
+  file.name2 <- paste(date, "-", event.name, "-", name, ".csv", sep="")
+  if (file.exists(file.name1) & file.exists(file.name2)) {
+    leg.data <- read.csv(file.name1)  
+    event.data <- read.csv(file.name2)  
+    
+    # HS
+    hs <- which(event.data$Name == "HS")
+    to <- which(event.data$Name == "TO")
+    
+    # HS
+    plot(leg.data$Timestamp, leg.data$ControlValueC, type = "l", xlab = "t(ms)", xaxs='i', main = paste("HS", date))
+    points(event.data$Timestamp[hs], event.data$Value[hs], col="red")
+    # TO
+    plot(leg.data$Timestamp, leg.data$ControlValueA, type = "l", xlab = "t(ms)", xaxs='i', main = paste("TO", date))
+    points(event.data$Timestamp[to], event.data$Value[to], col="blue")
+    
+    
+  } else {
+    print(paste(file.name1, " or ", file.name2, " does not exist"))
   }
-  
-  file.name <- paste(date, "-", event.name, "-", name, ".csv", sep="")
-  if (file.exists(file.name)) {
-    event.data <- read.csv(file.name)  
-  }
-  
-  # HS
-  hs <- which(event.data$Name == "HS")
-  to <- which(event.data$Name == "TO")
-  
-  # HS
-  plot(leg.data$Timestamp, leg.data$ControlValueC, type = "l", xlab = "t(ms)", xaxs='i', main = paste("HS", date))
-  points(event.data$Timestamp[hs], event.data$Value[hs], col="red")
-  # TO
-  plot(leg.data$Timestamp, leg.data$ControlValueA, type = "l", xlab = "t(ms)", xaxs='i', main = paste("TO", date))
-  points(event.data$Timestamp[to], event.data$Value[to], col="blue")
-  
 }
